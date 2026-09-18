616577d581c6776e133fda5fd6dcab2ef7ab7921
Joshua Wang <joshuawang9999@gmail.com>
2026-09-18 18:32:11 -0400

    first commit

---
+++ src/store.cpp
@@ -0,0 +1,11 @@
+#include "store.hpp"
+
+void KVStore::set(const std::string &key, const std::string &value)
+{
+}
+std::optional<std::string> KVStore::get(const std::string &key)
+{
+}
+bool KVStore::remove(const std::string &key)
+{
+}
---
+++ src/store.hpp
@@ -0,0 +1,13 @@
+#include <unordered_map>
+#include <string>
+
+class KVStore
+{
+private:
+    std::unordered_map<std::string, std::string> data;
+
+public:
+    void set(const std::string &key, const std::string &value);
+    std::optional<std::string> get(const std::string &key);
+    bool remove(const std::string &key);
+};
