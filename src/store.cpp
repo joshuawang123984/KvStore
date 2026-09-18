662962202359f78f8ec080e91899d26a965d3791
Joshua Wang <joshuawang9999@gmail.com>
2026-09-18 18:36:46 -0400

    default map operations

--- src/store.cpp
+++ src/store.cpp
@@ -1,11 +1,31 @@
 #include "store.hpp"
 
-void KVStore::set(const std::string &key, const std::string &value)
+bool KVStore::set(const std::string &key, const std::string &value)
 {
+    if (data.find(key) != data.end())
+    {
+        return false;
+    }
+
+    data[key] = value;
+    return true;
 }
 std::optional<std::string> KVStore::get(const std::string &key)
 {
+    if (data.find(key) != data.end())
+    {
+        return data[key];
+    }
+
+    return std::nullopt;
 }
 bool KVStore::remove(const std::string &key)
 {
+    if (data.find(key) != data.end())
+    {
+        data.erase(key);
+        return true;
+    }
+
+    return false;
 }
--- src/store.hpp
+++ src/store.hpp
@@ -7,7 +7,7 @@ private:
     std::unordered_map<std::string, std::string> data;
 
 public:
-    void set(const std::string &key, const std::string &value);
+    bool set(const std::string &key, const std::string &value);
     std::optional<std::string> get(const std::string &key);
     bool remove(const std::string &key);
 };
